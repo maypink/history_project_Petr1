71dfe5ae0317bb4f66072e08057e20d0afef8618
Nikita <nikitos3046@yandex.ru>
2021-11-11 13:39:40 +0300

    data added

--- reproduce/01-get_monuments_info.py
+++ reproduce/01-get_monuments_info.py
@@ -30,7 +30,7 @@ def get_coords(address: str):
     return {'lat': lat, 'lng': lng}
 
 
-def get_info(df: pd.DataFrame):
+def get_info(df: pd.DataFrame, offset_for_index = 0):
     monuments = []
     for i in tqdm(df.iterrows(), total = df.shape[0]):
         cur_url = 'http://petersmonuments.ru' + i[1].link
@@ -38,29 +38,28 @@ def get_info(df: pd.DataFrame):
         soup = BeautifulSoup(resp.text, 'lxml')
         name = soup.find_all('div', class_='full-description')[0].find_all('h1')[0].contents[0].strip()
         info = soup.find_all('div', class_='full-description__mini-desc')[0].find_all('p')
-        location = info[0].contents[2]
+        location = info[0].contents[2].strip()
 
         image_urls = []
         for image in soup.find_all('a', class_='gallery_monuments'):
-            image_urls.append(f"http://petersmonuments.ru/{image.attrs['href']}")
+            image_urls.append(f"http://petersmonuments.ru{image.attrs['href']}")
 
         type = ''
         if len(info) > 1:
-            type = info[1].contents[2]
-        type.strip()
+            type = info[1].contents[2].strip()
 
         status = ''
         if len(info) > 2:
-            status = info[2].contents[2]
-        status.strip()
+            status = info[2].contents[2].strip()
 
         desc_raw = soup.find_all('div', class_='full-description__text')[0].find_all('p')[1:-1]
         description = ''
         for par in desc_raw:
             if len(par) == 1:
-                description += par.contents[0]
+                description += par.contents[0].strip() + '\n'
         coords = get_coords(location)
-        monument = Monument(0, name, location, coords, type, status, remove_useless(description))
+        # coords = {'lat': 0.0, 'lng': 0.0}
+        monument = Monument(i[0] + offset_for_index, name, location, coords, type, status, remove_useless(description), image_urls.copy())
         monuments.append(monument)
     return monuments
 
@@ -75,40 +74,48 @@ def check_dir(path: Path, key = 1):
         os.mkdir(path)
 
 
-def save_info(root: Path, monuments: List[Monument]):
+def save_info(root: Path, monuments: List[Monument], is_start: bool = False, is_end: bool = False):
+
+    path_coords = os.path.join(root, 'processed', 'coords', "coords.json")
+    check_dir(os.path.join(root, 'processed', 'coords'), 2)
+    if is_start:
+        with open(path_coords, 'w', encoding='utf8') as file:   
+            file.write('[')
+
 
     for monument in monuments:
-        monument.id = id(monument)
         file_name = str(monument.id) + '.json'
         check_dir(root)
-
-        path_coords = os.path.join(root, 'processed', 'coords', file_name)
-        check_dir(os.path.join(root, 'processed', 'coords'), 2)
 
         path_info = os.path.join(root, 'processed', 'info', file_name)
         check_dir(os.path.join(root, 'processed', 'info'), 2)
 
         try:
-            with open(path_coords, 'w', encoding='utf8') as file:
+            with open(path_coords, 'a', encoding='utf8') as file:
                 json.dump(monument.coords_to_json(), file, ensure_ascii=False)
+                if not is_end or monuments.index(monument) != len(monuments) - 1:
+                    file.write(',')
 
             with open(path_info, 'w', encoding='utf8') as file2:
                 json.dump(monument.info_to_json(), file2, ensure_ascii=False)
 
         except OSError:
             pass
+    
+    if is_end:
+        with open(path_coords, 'a', encoding='utf8') as file:
+            file.write(']')
+
 
 if __name__ == '__main__':
 
-    rus_links = pd.read_csv('../data/raw/rus_links.csv')
-    eur_links = pd.read_csv('../data/raw/eur_links.csv')
-
-    rus_monuments = get_info(rus_links)
-    eur_monuments = get_info(eur_links)
-
     root = Path('../data/')
 
-    save_info(root, rus_monuments)
-    save_info(root, eur_monuments)
+    rus_links = pd.read_csv('../data/raw/rus_links.csv')
+    save_info(root, get_info(rus_links[0:100]), is_start=True)
+    for i in range(200, 1900, 100):
+        save_info(root, get_info(rus_links[i - 100:i]))
+    save_info(root, get_info(rus_links[1800:1809]))
 
-
+    eur_links = pd.read_csv('../data/raw/eur_links.csv')
+    save_info(root, get_info(eur_links, len(rus_links)), is_end=True)
